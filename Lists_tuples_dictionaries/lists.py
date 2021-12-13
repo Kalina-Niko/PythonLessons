b6a84a9ae76282c9612a5b75fa5f55ccc74d8436
mykytakalinichenko <niko.kalina@my.com>
2021-12-13 20:57:58 +0200

    list lessons 2

--- Lists_tuples_dictionaries/lists.py
+++ Lists_tuples_dictionaries/lists.py
@@ -32,3 +32,38 @@
 # objects = [1, 2.5, "привет", True, [1, 2, 3, 4], []]  # создание листа и наполнение его разными типами данных!
 # print(objects)
 # print(objects[4])  # ВЫвод значения из листа с помощью индекса!
+
+# objects = [1, 2.5, "привет", True, [1, 2, 3, 4], []]
+# print(objects)
+#
+# for i in objects:
+#     print(i)
+#
+# counter = 0
+# while counter < len(objects):
+#     print(objects[counter])
+#     counter += 1
+
+# numbers = [1, 21, 54, 21, 43, 2, 21]
+# print(numbers)
+# numbers.append("Nikita")  # добавляем в конец элемент !
+# print(numbers)
+# print(numbers.index(21))  # Вычесление индекса по значению!
+# numbers.pop(7)  # Удаление элемента по индексу
+# print(numbers)
+# numbers.reverse()  # Зеркальное отображение ЛИСТА!
+# print(numbers)
+# numbers.sort()  # сортирует по возрастанию
+# print(numbers)
+# numbers.count(21)  # Считает повторение значения в листе!
+# print(numbers.count(21))
+# numbers.insert(-2, "Nikita")  # Вставляет значение по индексу, в данном примере показано со знаком МИНУС!
+# print(numbers)
+# numbers.pop(5)  # Удаление по индексу НИКИТА для дальнейшей работы с функциями!
+# print(numbers)
+#
+# print("Длина нашего листа:", len(numbers))  # Вычесление количества значений или по правильному длины листа!
+# print(sorted(numbers))  # Сортировка от меньшего к большему!
+# print(min(numbers))  # Вычесление меньшего значения !
+# print(max(numbers))  # Вычесление найбольшего значения !
+
