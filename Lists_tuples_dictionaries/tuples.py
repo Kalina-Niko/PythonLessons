7fb2dac9eee514ab6ad1e80591f879216d0259c1
mykytakalinichenko <niko.kalina@my.com>
2021-12-19 11:12:52 +0200

    tuple lesson 1

---
+++ Lists_tuples_dictionaries/tuples.py
@@ -0,0 +1,46 @@
+# user = ("Nikita", 29)
+# print(user)
+
+# user = "Nikita", 29
+# print(user)
+
+# user = ("Nikita",)
+# print(user)
+
+# user_lists = ["Oksana", "Galja", "Evpatij"]
+# user_tuple = tuple(user_lists)
+# print(user_tuple)
+
+# user_tuple = ("Oksana", "Galja", "Evpatij", "Jora", "Branka", "Gogi")
+# print(user_tuple[:])
+
+# user = ("Pashka", 25, True)
+# name, age, is_married = user
+# print(name)
+# print(age)
+# print(is_married)
+
+# def get_user():
+#     name = "Paska"
+#     age = 25
+#     is_married = False
+#     return name, age, is_married
+#
+#
+# print(get_user())
+
+# user_tuple = ("Pashka", 25, True)
+# print(len(user_tuple))
+# print(type(user_tuple))
+
+# user_tuple = ("Pashka", 25, True)
+# counter = 0
+# while counter < len(user_tuple):
+#     print(user_tuple[counter])
+#     counter += 1
+#
+# for i in user_tuple:
+#     print(i)
+
+
+
